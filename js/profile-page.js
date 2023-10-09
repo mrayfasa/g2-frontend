48d82898606d53d3f72afa9fe9276178fc04a967
mrayfasa <rayfasa2@gmail.com>
2023-10-10 02:54:46 +0700

    href delete

--- js/profile-page.js
+++ js/profile-page.js
@@ -1,18 +1,17 @@
- document.getElementById('LogoutButton').addEventListener('click', function() {
-    window.location.href = '../account-page.html'
-    alert('Anda akan keluar.');
+document.getElementById("LogoutButton").addEventListener("click", function () {
+  window.location = "account-page.html";
+  alert("Anda akan keluar.");
 });
 
-function getProfileDetails(){
-    AjaxRequest(API_URL + "/v1/user/profile", 'GET',{}, (res) => {
-        const profile_name = document.getElementById("profile-name");
-        const profile_email = document.getElementById("profile-email");
-        profile_email.innerHTML = res.data.email;
-        profile_name.innerHTML = res.data.fullname;
-
-    })
+function getProfileDetails() {
+  AjaxRequest(API_URL + "/v1/user/profile", "GET", {}, (res) => {
+    const profile_name = document.getElementById("profile-name");
+    const profile_email = document.getElementById("profile-email");
+    profile_email.innerHTML = res.data.email;
+    profile_name.innerHTML = res.data.fullname;
+  });
 }
-function logoutButton(){
-    setCookie('token', '', -1);
+function logoutButton() {
+  setCookie("token", "", -1);
 }
-getProfileDetails()
+getProfileDetails();
